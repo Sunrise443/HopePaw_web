7ae5d72a24587df987c1d367019b497b0e834eb1
Sunrise443 <sunkensun0@gmail.com>
2025-11-15 23:33:34 +0300

    added map and form for map

--- frontend/src/pages/Basket.tsx
+++ frontend/src/pages/Basket.tsx
@@ -1,6 +1,7 @@
 import { ActionButton } from "../components/ActionButtons";
 import { Header } from "../components/Header";
 import ProductBasketMiniature from "../components/ProductBasketMiniature";
+import MapPicture from "../assets/map.png";
 
 type BusketItem = {
   id: number;
@@ -86,29 +87,24 @@ export default function Basket() {
 
       <h2 className="m-4 text-xl text-[#574C3A]">Выбрать приют</h2>
 
-      <div className="flex flex-col lg:flex-row justify-center gap-16 max-w-[90%] mx-auto px-4">
-        <div className="grid gap-4 flex-1">
-          {basketItems.map(({ id, name, vendor, price, imageUrl }) => (
-            <ProductBasketMiniature
-              id={id}
-              name={name}
-              vendor={vendor}
-              price={price}
-              imageUrl={imageUrl}
-            />
-          ))}
-          <div className="flex items-center bg-[#A0937D] text-[#EDE6DB] rounded-xl gap-3 px-4 py-2">
-            Дополнительная сумма пожертвований
+      <div className="flex flex-col lg:flex-row justify-center gap-16 max-w-[60%] mx-auto">
+        <div className="flex-1 max-w-md">
+          <form className="bg-[#A0937D] rounded-2xl p-3 flex flex-col gap-4">
             <input
-              type="number"
-              className="bg-[#EDE6DB] text-[#A0937D] ml-2 rounded-lg px-3 py-1 w-32 text-right outline-none"
+              className="rounded-xl py-2 px-4 text-[#A0937D] bg-[#EDE6DB] placeholder-[#84795F] outline-none"
+              placeholder="Город"
             />
-            руб.
-          </div>
+            <input
+              className="rounded-xl py-2 px-4 text-[#A0937D] bg-[#EDE6DB] placeholder-[#84795F] outline-none"
+              placeholder="Организация"
+            />
+          </form>
         </div>
 
-        <div className="flex-1 max-w-md">
-          <div className="bg-[#A0937D] rounded-2xl p-3 flex flex-col gap-4"></div>
+        <div className="grid gap-4 flex-1">
+          <div className="bg-[#A0937D] rounded-2xl p-3 flex flex-col gap-4">
+            <img src={MapPicture} alt="dfjgl" />
+          </div>
         </div>
       </div>
     </div>
