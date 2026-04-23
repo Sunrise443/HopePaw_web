a4d82c16bc104efd2200e25332a394f04fc9682e
Sunrise443 <sunkensun0@gmail.com>
2026-04-23 20:43:13 +0300

    chore: changed vite config for tests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,5 +14,6 @@ export default defineConfig({
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/setupTests.ts",
+    exclude: ["tests/**", "node_modules/**", "dist/**"],
   },
 });
